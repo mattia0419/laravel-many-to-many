910eba9e474e40168c25c5aadf21b5e2286d5601
mattia0419 <pironemattia6@gmail.com>
2023-10-28 20:21:09 +0200

    add migration, model and pivaut table

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Technology;
+
+use Faker\Generator as Faker;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $_technologies = ['html', 'css', 'bootstrap', 'sass', 'js', 'vue', 'php', 'mysql', 'laravel', 'markdown'];
+        foreach($_technologies as $_technology){
+            $technology = new Technology();
+            $technology->label = $_technology;
+            $technology->color = $faker->hexColor();
+            $technology->save();
+        }
+    }
+}
